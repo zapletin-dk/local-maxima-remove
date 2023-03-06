6ebd1ace840fbc5206a63f98c089819cc9b77870
Zapletin Daniil <Zaplot1994@gmail.com>
2023-03-06 21:49:51 +0300

    Names refactored

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -5,24 +5,22 @@ import java.util.Arrays;
 public class LocalMaximaRemove {
 
     public static int[] removeLocalMaxima(int[] array){
-        int []newArray = new int[array.length];
-        int count = 0;
-        if (array[0]<=array[1]){
-            newArray[count] = array[0];
-            count++;
+        int[] copyOfArray = new int[array.length];
+        int stepsCounter = 0;
+        if (array[0] <= array[1]){
+            copyOfArray[stepsCounter] = array[0];
+            stepsCounter++;
         }
         for (int i = 1; i < array.length-1; i++) {
-            if (array[i-1]>=array[i] || array[i]<=array[i+1]){
-                newArray[count] = array[i];
-                count++;
+            if (array[i-1] >= array[i] || array[i] <= array[i+1]){
+                copyOfArray[stepsCounter] = array[i];
+                stepsCounter++;
             }
         }
-        if (array[array.length-1]<=array[array.length-2]){
-            newArray[count] = array[array.length-1];
-            count++;
+        if (array[array.length-1] <= array[array.length-2]){
+            copyOfArray[stepsCounter] = array[array.length-1];
+            stepsCounter++;
         }
-        //put your code here
-        return Arrays.copyOf(newArray,count);
-        //throw new UnsupportedOperationException();
+        return Arrays.copyOf(copyOfArray,stepsCounter);
     }
 }
